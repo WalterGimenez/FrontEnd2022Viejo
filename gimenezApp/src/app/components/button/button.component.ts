0951ee3a0769e9b75f00e81343097aaa03b212cf
WalterGimenez <wallegimenez@gmail.com>
2022-08-04 10:53:08 -0300

    se corrigió el componente button y se agregó componente page-error

--- gimenezApp/src/app/components/button/button.component.ts
+++ gimenezApp/src/app/components/button/button.component.ts
@@ -6,9 +6,7 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./button.component.css']
 })
 export class ButtonComponent implements OnInit {
-  @Input() class! : string;
   @Input() text!: string;
-  @Input() type!: string;
   @Input() id!: string;
 
   constructor() { }
