9a1128c09d5add75e9f700ea715fee95e6c64dac
WalterGimenez <wallegimenez@gmail.com>
2022-08-04 13:21:46 -0300

    se agregaron los componenetes faltantes

--- gimenezApp/src/app/app-routing.module.ts
+++ gimenezApp/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
+import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PageErrorComponent } from './components/page-error/page-error.component';
 
 const routes: Routes = [
-  {path:'**', component: PageErrorComponent}
+  {path: "", component: AppComponent, pathMatch: "full"}//,
+  //{path:'**', component: PageErrorComponent}
 ];
 
 @NgModule({
--- gimenezApp/src/app/app.module.ts
+++ gimenezApp/src/app/app.module.ts
@@ -5,12 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ButtonComponent } from './components/button/button.component';
 import { PageErrorComponent } from './components/page-error/page-error.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AboutComponent } from './components/about/about.component';
+import { EducationComponent } from './components/education/education.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { ProyectComponent } from './components/proyect/proyect.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ButtonComponent,
-    PageErrorComponent
+    PageErrorComponent,
+    HeaderComponent,
+    AboutComponent,
+    EducationComponent,
+    ExperienceComponent,
+    ProyectComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
