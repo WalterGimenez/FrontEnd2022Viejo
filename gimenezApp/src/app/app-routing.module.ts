a560496ac889ce6c543c9524b6594e65f5c7e2ad
WalterGimenez <wallegimenez@gmail.com>
2022-08-05 14:04:51 -0300

    se terminó de construir el componente page-error y su ruta

--- gimenezApp/src/app/app-routing.module.ts
+++ gimenezApp/src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
+import { HomeComponent } from './components/home/home.component';
 import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PageErrorComponent } from './components/page-error/page-error.component';
 
 const routes: Routes = [
-  
-  
+  {path: "", component: HomeComponent, pathMatch: 'full'},
+  {path: "**", component: PageErrorComponent, pathMatch: 'full'} 
 ];
 
 @NgModule({
